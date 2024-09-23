433cbb5725a7309a6c6869491fe2cad5990623e9
Ashish Kumar <48367301+Ashishrock9394@users.noreply.github.com>
2024-09-23 09:48:58 +0530

    shayrana

---
+++ app.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
+
+const userRoute = require('./routes/user');
+const { checkForAuthenticationCookie } = require("./middleware/authentication");
+
+const app = express();
+const PORT = 8000;
+
+mongoose.connect('mongodb://127.0.0.1:27017/shayrana').then(e=>console.log("mongodb connected"));
+
+app.set("view engine", "ejs")
+app.set("views",path.resolve("./views"));
+
+app.use(express.urlencoded({ extended: false}));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
+
+
+app.get('/',(req, res)=>{
+    res.render("home",{
+        user: req.user,
+    });
+});
+
+app.use('/user', userRoute);
+
+
+app.listen(PORT, () => console.log(`Server started at PORT:${PORT}`));
