9951704fd45b514b0b44e895033bfe8fc5bb3826
Ashish Kumar <48367301+Ashishrock9394@users.noreply.github.com>
2024-09-27 12:31:20 +0530

    footer added

--- routes/blog.js
+++ routes/blog.js
@@ -41,7 +41,6 @@ router.post("/", upload.single("coverImage"), async(req, res) => {
   router.get('/:id', async (req, res)=> {
     const blog = await Blog.findById(req.params.id).populate('createdBy');
     const comments = await Comment.find({blogId:req.params.id}).populate('createdBy');
-    console.log(comments);
     return res.render("blog",{
         user: req.user,
         blog,
